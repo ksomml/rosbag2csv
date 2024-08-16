26599317a383b066e1f715bfb7374b1b5ff772ca
Michal Sojka <michal.sojka@cvut.cz>
2024-08-17 00:10:55 +0200

    Add flake.nix for testing the code with Nix (nix develop)

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs = {
+    nix-ros-overlay.url = "github:lopsided98/nix-ros-overlay/master";
+    nixpkgs.follows = "nix-ros-overlay/nixpkgs";  # IMPORTANT!!!
+  };
+  outputs = { self, nix-ros-overlay, nixpkgs }:
+    nix-ros-overlay.inputs.flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ nix-ros-overlay.overlays.default ];
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          name = "rosbag2csv";
+          packages = [
+            (with pkgs.rosPackages.humble; buildEnv {
+                paths = [
+                    ros-base
+                    rosbag2-storage-mcap
+                ];
+            })
+          ];
+        };
+      });
+  nixConfig = {
+    extra-substituters = [ "https://ros.cachix.org" ];
+    extra-trusted-public-keys = [ "ros.cachix.org-1:dSyZxI8geDCJrwgvCOHDoAfOm5sV1wCPjBkKL+38Rvo=" ];
+  };
+}
